1e416d9fa543cac250ba867b32b7548fcffb3708
AJPadillo <98108563+AJPadillo@users.noreply.github.com>
2024-07-12 10:33:52 +0000

    actualizado 1

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,69 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
+			personas: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
+				}
+			],
+			planetas: [
 				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
+				}
+			],
+			vehiculos: [
+				{
 				}
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+				getActions().getCharactersInfo();
+			  },
+			// Use getActions to call a function within a fuction
 			loadSomeData: () => {
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
-};
+			getCharacters: async () => {
+				try {
+					const response = await fetch("https://www.swapi.tech/api/people/")
+					const data = await response.json();
+					const personas = data.results.map((character) => getActions().getCharactersInfo(character.uid));
+					setStore({ personas: personas });
+				} catch (error) {
+					console.log(error);
+				}
+			},
+			getCharactersInfo: async (id) => {
+				try {
+					const response = await fetch(`https://www.swapi.tech/api/people/${id}`)
+					const data = await response.json();
+					console.log(data);
+					return data.results;
+				} catch (error) {
+					console.log(error);
+				}
+			},
+			getPlanets: async () => {
+				try {
+					const response = await fetch("https://www.swapi.tech/api/planets/")
+					const data = await response.json();
+					setStore({ planetas: data.results });
+				} catch (error) {
+					console.log(error);
+				}
+			},
+			getVehicles: async () => {
+				try {
+					const response = await fetch("https://www.swapi.tech/api/vehicles/")
+					const data = await response.json();
+					setStore({ vehiculos: data.results });
+				} catch (error) {
+					console.log(error);
+				}
+			},
+		},
+	}
+}
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,26 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import { Context } from "../store/appContext";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const navigate = useNavigate();
+	return (
+		<>
+			<ul className="list-group">
+				{store.personas.map((item, index) =>
+					<div className="container border px-0" key={index}>
+						<div className="row my-2">
+							<div className="col-md-6">
+								<h3>{item.name}</h3>
+								<p className="mb-1">{item.mass}</p>
+							</div>
+						</div>
+					</div>
+				)}
+			</ul>
+		</>
+	)
+}
