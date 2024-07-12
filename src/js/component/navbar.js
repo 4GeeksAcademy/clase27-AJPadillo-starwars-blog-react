5a3f3f2c1436a3630080f6f885b68998db7a9ae9
AJPadillo <98108563+AJPadillo@users.noreply.github.com>
2024-07-12 18:47:01 +0000

    actualizado 6

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,21 +7,18 @@ export const Navbar = () => {
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz1oxYa16QkYbSAjstrwIqAeIOoRjZuIM4ow&s" style={{ width: "150px" }} /></span>
 			</Link>
-			<div className="ml-auto me-3">
-				<div className="btn-group">
-					<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-						Favorites
-					</button>
-					<ul className="dropdown-menu">
-						<li><a className="dropdown-item" href="#">Action</a></li>
-						<li><a className="dropdown-item" href="#">Another action</a></li>
-						<li><a className="dropdown-item" href="#">Something else here</a></li>
-						<li><hr className="dropdown-divider" /></li>
-						<li><a className="dropdown-item" href="#">Separated link</a></li>
-					</ul>
-				</div>
-
+			<div class="btn-group dropstart me-3">
+				<button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					Dropstart
+				</button>
+				<ul class="dropdown-menu">
+					<li><a class="dropdown-item" href="#">Menu item</a></li>
+					<li><a class="dropdown-item" href="#">Menu item</a></li>
+					<li><a class="dropdown-item" href="#">Menu item</a></li>
+				</ul>
 			</div>
+
+
 		</nav>
 	);
 };
